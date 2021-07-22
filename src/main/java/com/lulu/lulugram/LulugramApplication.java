3e3f3d3f394a8e6544e9c84e986761d64ae4bac6
YeonSeung Choi <yseung777@mailplug.co.kr>
2021-07-22 19:21:06 +0900

    spring boot add

---
+++ src/main/java/com/lulu/lulugram/LulugramApplication.java
@@ -0,0 +1,13 @@
+package com.lulu.lulugram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LulugramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LulugramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lulu/lulugram/LulugramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lulu.lulugram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LulugramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
