83dbd2f1f186f097fa5f426f33c453543112e868
YeonSeung Choi <yseung777@mailplug.co.kr>
2021-07-23 10:51:39 +0900

    자동배포 테스트

--- src/main/java/com/lulu/lulugram/controller/HelloController.java
+++ src/main/java/com/lulu/lulugram/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("api/hello")
     public String hello() {
-        return "hello test123123!! hello!!!";
+        return "hello hello is anybody here????";
     }
 }
